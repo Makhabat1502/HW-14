de123446ff73521ba84ade40f9ba1faa23a71bf5
Makhabat Berdibekova <makhabat.berdibekova.kk@gmail.com>
2022-03-03 14:28:17 +0600

    finish

---
+++ js/js.js
@@ -0,0 +1,5 @@
+const toggler = document.querySelector(".menu-toggler").addEventListener("click", function () {
+    menu.classList.toggle("menu-show");
+  });
+
+const menu = document.querySelector(".menu");
